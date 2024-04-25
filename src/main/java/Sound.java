9565e30d2fdd8f734c5c8b1ad515a3a7a6b78301
KYCHU1 <142847721+KYCHU1@users.noreply.github.com>
2024-04-24 20:01:16 -0700

    Update Sound.java

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -17,7 +17,7 @@ public class Sound
   {  
     int count = 0;
     for(int i = 0; i < samples.length; i++){
-      if(samples[i] > limit{
+      if(samples[i] > limit){
         samples[i] = limit;
         count++;
       } else if (samples[i] < -limit){
